990e45196b15b09bca364a43184440c66affc3b1
Stan Tsvek <stas.tsvek@gmail.com>
2022-10-18 00:27:45 +0300

    feat: implement stack solution

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+	arr = [];
+
+    constructor() {
+			this.arr = [];
+    }
+		
+  push(element) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+		this.arr.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+		return this.arr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+		return this.arr.at(-1);
   }
 }
 
